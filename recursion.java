29f9e9411de8e9b44b65a7f62aaa7162fb86d625
akshay kumar <akshay.1904kumar@gmail.com>
2025-05-27 11:52:06 +0530

    more six questions

--- recursion.java
+++ recursion.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 
 public class recursion {
     public static void main(String[]args){
-        int[]array = new int[]{5,6};
-        System.out.println(binaryRecursive(array,4,0,array.length-1));
+        char[]array = {'h','a','n','n','a','h'};
+        reverse_charArrayOne(array,0,array.length-1);
+//        System.out.println(Arrays.toString(array));
+//        printNo(9);
+//        System.out.println(lengthString("GEEKSFORGEEKS",0,0));
+        System.out.println(specialFibo(1,3,2,6));
+
     }
 
 
@@ -48,5 +53,81 @@ public class recursion {
         return binaryRecursive(array,target,start,middle-1);
 
     }
+
+    static void findFirst(String s,int index){
+        if(index==s.length()){
+            return;
+        }
+        if(s.charAt(index)>=65 && s.charAt(index)<=97){
+            System.out.println(s.charAt(index));
+            return;
+        }
+        findFirst(s,index+1);
+    }
+
+    static void reverse_charArrayOne(char[]array,int start,int end){
+        if(start>=end)return;
+        char temp = array[start];
+        array[start]=array[end];
+        array[end]=temp;
+        reverse_charArrayOne(array,start+1,end-1);
+    }
+
+    public static void printNo(int n){
+        helper(1,n);
+    }
+
+    private static void helper(int first, int n) {
+        if(first==n) {
+            System.out.println(first);
+            return;
+        };
+        System.out.println(first);
+        helper(first+1,n);
+    }
+
+
+    static int fibo(int n){
+        int ans =0;
+        if(n<=1)return n;
+        ans = fibo(n-1)+fibo(n-2);
+        return ans;
+    }
+
+    /*
+    static int specialFibo(int a,int b,int n){
+        if(n==0)return a;
+        if(n==1)return b;
+        int ans;
+        ans = specialFibo(a,b,n-1) ^ specialFibo(a,b,n-2);
+        return ans;
+    }
+
+     */
+
+    static int lengthString(String s,int given,int length){
+        if(given==s.length()){
+            return length;
+        }
+        if(s.charAt(given)!=' '){
+            length++;
+        }
+        return lengthString(s,given+1,length);
+    }
+
+
+    static int specialFibo(int a,int b,int c,int n){
+        if(n==1){
+            return a;
+        }
+        if(n==2)return b;
+        if(n==3)return c;
+
+        int ans =0;
+        ans = specialFibo(a,b,c,n-1) + specialFibo(a,b,c,n-2)+ specialFibo(a,b,c,n-3);
+        return ans;
+
+    }
+
 
 }
