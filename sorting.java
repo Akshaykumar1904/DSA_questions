d3961ac1631fb78e7a6c918270e1f8632f54e79e
akshay kumar <akshay.1904kumar@gmail.com>
2025-06-26 01:25:10 +0530

    questions on binary search

--- sorting.java
+++ sorting.java
@@ -10,7 +10,10 @@ public class sorting {
 //        System.out.println(thirdMax(new int[]{1,2,2}));
 //        System.out.println(lengthOfLIS(new int[]{0,1,0,3,2,3}));
 //        System.out.println(SearchInSorted(new int[]{5,1,3},5));
-        System.out.println(shipWithinDays(new int[]{3,2,2,4,1,4},3));
+//        System.out.println(shipWithinDays(new int[]{3,2,2,4,1,4},3));
+//        System.out.println(findPeakElement(new int[]{1,2,1,3,5,6,4}));
+//        System.out.println(fixedPoint(new int[]{0,2,5,8,7}));
+        System.out.println(minEatingSpeed(new int[]{312884470},312884469));
 
     }
 
@@ -214,4 +217,105 @@ public class sorting {
         return max;
     }
 
+
+    public static int findPeakElement(int[] nums) {
+        int start = 0, end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // you're in the decreasing part → peak is to the left or at mid
+                end = mid;
+            } else {
+                // you're in the increasing part → peak is to the right
+                start = mid + 1;
+            }
+        }
+        return start; // or return end; both are same
+    }
+
+    public static int findMin(int[] nums) {
+        if(findPivotIndex(nums)==-1){
+        return nums[0];
+        }
+        return nums[findPivotIndex(nums)+1];
+    }
+
+    private static int findPivotIndex(int[]nums){
+        int start =0;
+        int end = nums.length-1;
+        while(start<=end){
+            int middle = start+(end-start)/2;
+            if(middle+1 <nums.length && nums[middle] > nums[middle+1]){
+                return middle;
+            }else if(middle >start && nums[middle - 1] > nums[middle]){
+                return middle-1;
+            }else if(nums[middle] <= nums[start]){
+                end = middle-1;
+            }else{
+                start = middle+1;
+            }
+        }
+        return -1;
+    }
+
+    public static int fixedPoint(int[]nums){
+        int start =0;
+        int end = nums.length-1;
+        while(start<=end){
+            int middle = start +(end-start)/2;
+            if(nums[middle] == middle)return middle;
+            else if(nums[middle] > middle){
+                end = middle-1;
+            }else {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int minEatingSpeed(int[] weights, int h) {
+        int low = 1;
+        int high = highest(weights);
+        int ans = high;
+        while(low<=high){
+            int middle = low +(high-low)/2;
+            if(isValid(weights,h,middle)){
+                ans = middle;
+                high = middle -1;
+            }else{
+                low = middle +1;
+            }
+        }
+        return ans;
+    }
+
+    private static boolean isValid(int[] arr, int h,int value) {
+        long sum=0;
+        for(int num:arr){
+            sum += (long)Math.ceil((double) num /value);
+        }
+        return sum <= h;
+    }
+
+//    public static int lowest(int[] arr){
+//        int min=Integer.MAX_VALUE;
+//        for(int num:arr){
+//            if(num<min){
+//                min = num;
+//            }
+//        }
+//        return min;
+//    }
+
+    public static int highest(int[] arr){
+        int min=Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>min){
+                min = num;
+            }
+        }
+        return min;
+    }
 }
