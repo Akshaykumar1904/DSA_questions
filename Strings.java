1956abf1b524f0e956a269748e34af165de9a66e
akshay kumar <akshay.1904kumar@gmail.com>
2025-06-27 01:14:49 +0530

    questions on strings

--- Strings.java
+++ Strings.java
@@ -2,7 +2,10 @@ package git_practice;
 
 public class Strings {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(" "));
+//        System.out.println(isPalindrome(" "));
+//        System.out.println(strStr("nothing","sad"));
+//        System.out.println(reverseWords("  hello world  "));
+        System.out.println(validPalindrome("abca"));
     }
 
     public static boolean isPalindrome(String s) {
@@ -27,6 +30,61 @@ public class Strings {
             right--;
         }
 
+        return true;
+    }
+
+
+    public static int strStr(String given,String toFind){
+        int i=0;
+        String search ="";
+        while(i<=given.length()-1 && i+toFind.length()<= given.length()){
+            search = given.substring(i,i+toFind.length());
+            if(toFind.equals(search)){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static String reverseWords(String s) {
+    String another = s.trim().replaceAll("\\s+", " ");
+    String[]words = another.split(" ");
+    StringBuilder sb = new StringBuilder();
+    for(int i= words.length-1;i>=0;i--){
+        sb.append(words[i]);
+        if(i!=0)sb.append(" ");
+    }
+    return sb.toString();
+    }
+
+    public static boolean validPalindrome(String s) {
+        int start =0;
+        int end = s.length()-1;
+        while(start<=end){
+            if(s.charAt(start)!= s.charAt(end)){
+                return isPalindromeString(s, start + 1, end) || isPalindromeString(s, start, end- 1);
+            }
+            else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+
+    }
+
+
+
+    private static boolean isPalindromeString(String s,int start,int end){
+        while(start<=end){
+            if(s.charAt(start) == s.charAt(end)){
+                start ++;
+                end --;
+            }else{
+                return false;
+            }
+        }
         return true;
     }
 }
